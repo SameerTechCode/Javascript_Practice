beb47f76ed8b4c3cf391d6080a68b0fed503a7bb
SameerTechCode <sameerkhattak0347@gmail.com>
2024-09-06 16:14:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+
+// ASSIGNMENT //
+// CHATPTER # 1
+// Q1
+// alert("FirstName");
+// alert("LastName");
+// alert("Email");
+// alert("Phone Number V. Password");
+// Q2
+// alert("Yor're Learning Javascript!");
+// Q3
+// alert("Welcome to the world of JavaScript!");
+
+// CHAPTER # 2
+// Q1
+// let fullName;
+// Q2 
+// let student;
+// student = "Sameer";
+// Q3
+// let teamname;
+// alert("Team Name.")
+// Q4
+// var bestman ="Charlie";
+// bestname="Khan";
+
+// CHAPTER # 3
+// Q1
+// var caseQty;
+// // Q2
+// caseQty =  144;
+// // Q3
+// var num = 9;
+// var result = num + 9;
+// Q4
+// let num = 24;
+// num = 2;
+// Q5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+// Q6
+// let num1 = 20;
+// num1 = 10 +15;
+// alert(num1)
+
---
+++ script.js
@@ -0,0 +1,78 @@
+// CHAPTER # 1
+// Q1
+// alert("Welcome to our website! We hope you have a great experience.");
+// Q2
+// alert("Error! Please Enter a valid Password");
+// Q3
+// alert("Wellcome to JS Land! \n happy Coding!")
+// Q4
+// alert("Wellcome to JS Land")
+// alert("Prevent this page from creating  additionl dialog")
+// Q5
+// console.log("Hello..I can run JS throught my web browser's console");
+// Q6
+// Q7
+// CHAPTER # 2
+// Q1
+// let username;
+// Q2
+// const myname = "Sameerkhattak";
+// Q3
+// let message = "Hello World";
+// alert(message)
+// Q4
+// alert("Jhone deo")
+// alert("15 year old ")
+// alert("Certified Mobile Apllication Development")
+// Q5
+// alert("Pizza \n Pizz \n Piz \n Pi \n P")
+// Q6
+// let email = "Sameerkhattak0347@Gmail.com";
+// alert(`My email address is ${email}`);
+// Q7
+// let Book = "A smarter way to learn JavaScript";
+// alert(`I am trying to learn from the Book ${Book}`)
+// Q8
+// document.write("YAH! I Can write HTMl Content thorugh Javascript")
+// Q9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// CHAPTER # 3 
+// Q1
+// let age = 20;
+// alert(`I am ${age} year old`);
+// Q2
+// let visitTime = 14;
+// alert(`You have visited this site ${visitTime} times.`);
+// Q3
+// let birthyear = 1990;
+// document.write(`My Birth Year is ${birthyear} <br> data type of my declared variable is my Number `);
+// Q4
+// let Visitorname =  "John Deo";
+// let productTitle = "T-Shirt";
+// let Quantity = 5;
+// alert(`${<b>Visitorname</b>}ordered ${<b>Quantity</b>} ${<b>productTitle</b>}(s) on XYZ Clohing store`);
+// CHAPTER#4
+// Q1
+// let student="sameer",age = 20,Cms =2915;
+// Q2
+// Legal
+// let my$name;
+// let myname123;
+// let my$name12;
+// let $myname;
+// let my12name;
+// Illegal
+// let 12myname;
+// let @myname;
+// let 12Myname12;
+// let function;
+// let user-name;
+
+// Q3
+// document.write(<h1>Rules For Naming JS Variables</h1>)
+// document.write("Variable names can only contain, numbers,$ and _forexample, $my_1stVariable")
+// document.write("Variables must begin with a letter, $ or . For example $name, _name or name")
+// document.write(" Variable names are case Sensitive")
+// document.write("Variable names should not be JS Keyword")
+
